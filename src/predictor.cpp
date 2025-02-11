756f28ca2b63a97e97fe0b46f9638575a8a19ec1
rqin525 <ruiqian.qin@gmail.com>
2025-02-11 09:44:46 -0800

    tournament functions added (need testing)

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -12,9 +12,9 @@
 //
 // TODO:Student Information
 //
-const char *studentName = "TODO";
-const char *studentID = "TODO";
-const char *email = "TODO";
+const char *studentName = "Richard Qin";
+const char *studentID = "A69031813";
+const char *email = "riqin@ucsd.edu";
 
 //------------------------------------//
 //      Predictor Configuration       //
@@ -29,6 +29,10 @@ int ghistoryBits = 17; // Number of bits used for Global History
 int bpType;            // Branch Prediction Type
 int verbose;
 
+int lhistoryBits = 10; // number of bits for local history
+int pcIndexBits = 10; // bits to index LHT
+int muxBits = 12; // bits for mux
+
 //------------------------------------//
 //      Predictor Data Structures     //
 //------------------------------------//
@@ -37,8 +41,16 @@ int verbose;
 // TODO: Add your own Branch Predictor data structures here
 //
 // gshare
-uint8_t *bht_gshare;
-uint64_t ghistory;
+uint8_t *bht_gshare; // global predictor
+uint64_t ghistory; // GHR: tracks outcomes of last N branches
+// Tournament
+uint8_t *bht_tourney; // global predictor
+// use same GHR
+uint8_t *lht; // LHT: store recent outcomes specific to branch (pc)
+uint8_t *local_pht;
+uint8_t *mux; // choose between global and local predictor;
+              // 0=strong local, 1=weak local, 2=weak global, 3=strong global
+
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -115,9 +127,109 @@ void train_gshare(uint32_t pc, uint8_t outcome)
   ghistory = ((ghistory << 1) | outcome);
 }
 
+// Tournament functions
+void init_tournament()
+{
+  int bht_entries = 1 << ghistoryBits;
+  int lht_entries = 1 << pcIndexBits;
+  int mux_entries = 1 << muxBits;
+
+  // table setup
+  bht_tourney = (uint8_t *)malloc(bht_entries * sizeof(uint8_t));
+  local_pht = (uint8_t *)malloc(bht_entries * sizeof(uint8_t));
+  lht = (uint8_t *)malloc(lht_entries * sizeof(uint8_t));
+  mux = (uint8_t *)malloc(mux_entries * sizeof(uint8_t));
+
+  // init predictors (weakly not taken)
+  for (int i = 0; i < bht_entries; i++) {
+    bht_tourney[i] = WN;
+    local_pht[i] = WN;
+  }
+  for (int i = 0; i < lht_entries; i++) {
+    lht[i] = 0;
+  }
+
+  // init mux (weak local predictor)
+  for (int i = 0; i < mux_entries; i++) {
+    mux[i] = 1;
+  }
+  ghistory = 0;
+}
+
+uint8_t tournament_predict(uint32_t pc)
+{
+  uint32_t bht_entries = 1 << ghistoryBits;
+  uint32_t lht_entries = 1 << pcIndexBits;
+  uint32_t mux_entries = 1 << muxBits;
+
+  uint32_t global_index = (pc ^ ghistory) & (bht_entries - 1);
+  uint32_t local_index = lht[pc & (lht_entries - 1)] & (bht_entries - 1);
+  uint32_t mux_index = pc & (mux_entries - 1);
+
+  // predict!
+  uint8_t global_pred = (bht_tourney[global_index] >= WT) ? TAKEN : NOTTAKEN;
+  uint8_t local_pred = (local_pht[local_index] >= WT) ? TAKEN : NOTTAKEN;
+  uint8_t choice = mux[mux_index];
+
+  // choose between predictors
+  return (choice >= 2) ? global_pred : local_pred;
+}
+
+void train_tournament(uint32_t pc, uint8_t outcome)
+{
+  uint32_t bht_entries = 1 << ghistoryBits;
+  uint32_t lht_entries = 1 << pcIndexBits;
+  uint32_t mux_entries = 1 << muxBits;
+
+  uint32_t global_index = (pc ^ ghistory) & (bht_entries - 1);
+  uint32_t local_index = lht[pc & (lht_entries - 1)] & (bht_entries - 1);
+  uint32_t mux_index = pc & (mux_entries - 1);
+
+  // predict!
+  uint8_t global_pred = (bht_tourney[global_index] >= WT) ? TAKEN : NOTTAKEN;
+  uint8_t local_pred = (local_pht[local_index] >= WT) ? TAKEN : NOTTAKEN;
+
+  // global update state
+  if (outcome == TAKEN) {
+    if (bht_tourney[global_index] < ST) bht_tourney[global_index]++;
+  } else {
+    if (bht_tourney[global_index] > SN) bht_tourney[global_index]--;
+  }
+
+  // local update state
+  if (outcome == TAKEN) {
+    if (local_pht[local_index] < ST) local_pht[local_index]++;
+  } else {
+    if (local_pht[local_index] > SN) local_pht[local_index]--;
+  }
+
+  // mux update state
+  if (global_pred == outcome && local_pred != outcome) {
+    if (mux[mux_index] < 3) mux[mux_index]++;
+  } else if (global_pred != outcome && local_pred == outcome) {
+    if (mux[mux_index] > 0) mux[mux_index]--;
+  }
+
+  // update LHT
+  // (old bits) | outcome
+  // bit mask ((1 << lhistoryBits) - 1) to make sure does not exceed bit width
+  lht[pc & (lht_entries - 1)] = ((lht[pc & (lht_entries - 1)] << 1) | outcome);
+
+  // update global history register
+  ghistory = ((ghistory << 1) | outcome);
+}
+
 void cleanup_gshare()
 {
   free(bht_gshare);
+}
+
+void cleanup_tournament()
+{
+  free(bht_tourney);
+  free(local_pht);
+  free(lht);
+  free(mux);
 }
 
 void init_predictor()
@@ -130,6 +242,7 @@ void init_predictor()
     init_gshare();
     break;
   case TOURNAMENT:
+    init_tournament();
     break;
   case CUSTOM:
     break;
@@ -153,7 +266,7 @@ uint32_t make_prediction(uint32_t pc, uint32_t target, uint32_t direct)
   case GSHARE:
     return gshare_predict(pc);
   case TOURNAMENT:
-    return NOTTAKEN;
+    return tournament_predict(pc);
   case CUSTOM:
     return NOTTAKEN;
   default:
@@ -180,7 +293,7 @@ void train_predictor(uint32_t pc, uint32_t target, uint32_t outcome, uint32_t co
     case GSHARE:
       return train_gshare(pc, outcome);
     case TOURNAMENT:
-      return;
+      return train_tournament(pc, outcome);
     case CUSTOM:
       return;
     default:
